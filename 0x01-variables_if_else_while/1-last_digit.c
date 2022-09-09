e28afcf098224626378e8b5b839a5e455ceab3b8
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-09 13:15:52 -0700

    Last digt

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *Entry point
+ *main
+ */
+
+int main(void)
+{
+	int n;
+	int mod;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	mod = n % 10;
+
+	printf("Last digit of %i is %i ", n, mod);
+		
+	if (mod == 0)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (mod > 5)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	
+	return (0);
+}
