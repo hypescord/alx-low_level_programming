a64663af8077ebf096ca08eec89ed1f82b2fadf3
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-10 05:52:49 -0700

    using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
