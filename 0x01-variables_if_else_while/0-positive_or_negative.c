7d2c58a7b4aeaad65ef66399e047b70c3aa290b6
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-11 03:39:00 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -25,6 +25,6 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	
+
 	return (0);
 }
