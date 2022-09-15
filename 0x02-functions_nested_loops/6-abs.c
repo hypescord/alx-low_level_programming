1e84cc270194656ef191bf308cb7cb3aeaf8cd31
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-15 08:11:49 -0700

    return absolute value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,16 +2,18 @@
 
 /**
  * _abs - compute absolute value of integer
- * @n: integer argument
- * Return: absolute value
+ * @n: character to be tested
+ * Return: integer
  */
 
 int _abs(int n)
 {
-	if (n > 0)
-		return (n);
-	else if (n < 0)
+	if (n < 0)
+	{
 		return (-n);
-
-	return (0);
+	}
+	else
+	{
+		return (n);
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,4 +20,6 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int);
+
 #endif
