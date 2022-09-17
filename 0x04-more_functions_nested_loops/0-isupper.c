0b12aa7a3113d4aa1f857d3834398fe04bac55c3
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-17 06:39:54 -0700

    check if uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: character to be checked
- * Return: 1 if true, 0 if false
+ * _isupper - check for uppercase character
+ * @c: character element
+ * Return: 1 for upper, else 0 
  */
-
-int _isupper(int c)
+int _isupper(int c);
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 
 	return (0);
 }
