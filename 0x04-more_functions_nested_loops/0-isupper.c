d86d9d076d7a196be9b88e057112794fd87b96d4
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-17 06:46:28 -0700

    check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,13 @@
 /**
  * _isupper - check for uppercase character
  * @c: character element
- * Return: 1 for upper, else 0 
+ * Return: 1 for upper, else 0
  */
+
 int _isupper(int c);
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 
 	return (0);
 }
