ba6c2fe4781869736b97a4a5027905ad41f3c16a
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-17 07:00:31 -0700

    checks for a digit element

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit 0 through 9
- * @c: digit to be checked
- * Return: 1 if true, 0 if false
+ * _isdigit - check for a digit
+ * @c: integer element
+ * Return: 1 if digit, else 0
  */
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 0 && c <= 9)
 		return (1);
-
+	
 	return (0);
 }
