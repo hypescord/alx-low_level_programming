b423c0e0e9a01174b70283834ebdddd2f7d8d015
Ogunleye John Adegoke <hypescord@gmail.com>
2022-09-30 16:55:32 -0700

    Print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - print file name
+ *@argc: counter
+ *@argv: elements
+ *Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This readme for args for main
